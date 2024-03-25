81218ee5bb6c0269d90354afe8e7b32f4320c02c
Agnes <agnesajema@gmail.com>
2024-03-25 08:33:54 +0300

    add files

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+fun main(){
+ println (countOccurences())
+}
+
+fun isPalindrome(str: String,ch:Char):Int{
+ var character = 'b'
+    val countOccurences = isPalindrome("Barnie bakes brown bagels and buns",'b')
+return countOccurences("Barnie bakes brown bagels and buns",'b')
+
+
+}
+fun isPalindrome (word: String):Boolean{
+    var word ="wow"
+    var word2 ="wow"
+    var word3= "kayak"
+    if (word == (word)
+        println (true)
+
+       else
+    println(false)
+}
+
+fun arrayOf(num1:Int,num2:Int):{
+    var sum = num1+num2
+    return (sum)
+    var average = num1 ;num2
+}
+
